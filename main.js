46a36cce9a10554da6f6e575d357ff5a7f274e5c
Aryadolphin <78508957+arya0402@users.noreply.github.com>
2022-06-06 17:28:06 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ camera = document.getElementById("camera");
 Webcam.attach("#camera");
 
 function capture_image() {
-    Webcam.snap(function(data_uri){
+    Webcam.snap(function (data_uri) {
         document.getElementById("result").innerHTML = "<img id='captured_img' src='" + data_uri + "'>";
     });
 }
@@ -25,9 +25,46 @@ function modelLoaded() {
 }
 
 function speak() {
-var synth = window.speechSynthesis;
-speak_data_1 = "the first prediction is " + predicition_1;
-speak_data_2 = "and the second prediction is " + predicition_2;
-var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
-synth.speak(utterThis);
+    var synth = window.speechSynthesis;
+    speak_data_1 = "the first prediction is " + predicition_1;
+    speak_data_2 = "and the second prediction is " + predicition_2;
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+    synth.speak(utterThis);
+}
+
+function predict_emotion() {
+    img = document.getElementById("captured_img");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if(error) {
+        console.error(error);
+    }
+    else {
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML = results[0].label;
+        document.getElementById("result_emotion_name_2").innerHTML = results[1].label;
+        predicition_1 = results[0].label;
+        predicition_2 = results[1].label;
+        speak();
+        if(predicition_1 == "happy") {
+            document.getElementById("result_emoji").innerHTML = "&#128522;";
+        }
+        if(predicition_1 == "sad") {
+            document.getElementById("result_emoji").innerHTML = "&#128532;";
+        }
+        if(predicition_1 == "angry") {
+            document.getElementById("result_emoji").innerHTML = "&#128548;";
+        }
+        if(predicition_2 == "happy") {
+            document.getElementById("result_emoji_2").innerHTML = "&#128522;";
+        }
+        if(predicition_2 == "sad") {
+            document.getElementById("result_emoji_2").innerHTML = "&#128532;";
+        }
+        if(predicition_2 == "angry") {
+            document.getElementById("result_emoji_2").innerHTML = "&#128548;";
+        }
+    }
 }
